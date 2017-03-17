825ba2414069a3adba71d353caa82e79367d3c99
Ethan Glasser-Camp <ethan@betacantrips.com>
2017-03-17 15:29:44 -0400

    sign-xpi: flake8 cleanup

--- lambda/main.py
+++ lambda/main.py
@@ -4,28 +4,9 @@ sign-xpi: Sign an XPI file
 
 import os.path
 import sys
-sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "site-packages"))
+DIR = os.path.dirname(os.path.abspath(__file__))
+SITE_PACKAGES = os.path.join(DIR, "site-packages")
+sys.path.insert(0, SITE_PACKAGES)
 
-import logging
-import requests
-
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-
-def handle(event, context):
-    """
-    Lambda handler
-    """
-    logger.info("%s - %s", event, context)
-
-    url = "https://api.ipify.org?format=json"
-
-    raw = requests.get(url)
-    logger.info("%s", raw)
-    result = raw.json()
-
-    logger.info("Lambda IP: %s", result['ip'])
-
-    return event
+from sign_xpi import handle  # noqa: E402
+__all__ = ['handle']
