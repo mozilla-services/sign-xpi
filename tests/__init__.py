43bc604265bffd881a594be63a40b041d5181f20
Ethan Glasser-Camp <ethan@betacantrips.com>
2017-07-13 14:03:24 -0400

    Separate functional tests from the rest
    Travis only provides go 1.4 in the Python test environment. To use
    1.7, you have to use a Go test environment. Follow kinto-signer in
    extracting this sort of test into a separate file and running it in a
    separate environment.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,10 @@
 """Unit test package for sign_xpi package."""
+
+import os.path
+
+TEST_DIR, _ = os.path.split(__file__)
+ADDON_FILENAME = 'hypothetical-addon-unsigned.xpi'
+
+
+def get_test_file(filename):
+    return os.path.join(TEST_DIR, filename)
