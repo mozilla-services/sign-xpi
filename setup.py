a8384c5f9b6861e72b3277b6d0a234c6d760e4cf
Ethan Glasser-Camp <ethan@betacantrips.com>
2017-07-13 17:15:11 -0400

    Clean up packaging stuff

--- setup.py
+++ setup.py
@@ -2,8 +2,25 @@
 
 from setuptools import setup
 
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('CHANGELOG.rst') as changelog_file:
+    changelog = changelog_file.read()
+
 setup(
     name='sign_xpi-parent',
     version='0.1.0',
     description='Fake parent package for the sign-xpi lambda',
+    long_description=readme + '\n\n' + changelog,
+    author="Product Delivery Team",
+    author_email="storage-team@mozilla.com",
+    url='https://github.com/mozilla-services/sign-xpi',
+    license="MPL",
+    keywords='sign_xpi',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+    ],
+    zip_safe=False,
 )
